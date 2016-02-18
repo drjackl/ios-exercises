1032609bccf0cd871df84801011b923c4a0df611
Jack Li <drjackl@gmail.com>
2016-02-18 08:21:49 -0800

    Finished assignment 11 If-Else. Took longer than previous ones having to read the tests carefully

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,7 +15,19 @@
 
     /* WORK HERE */
     
-    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+//    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+    
+    if (dollars < 5)
+        itemToReturn = @"get out of my store";
+    else if (dollars < 6)
+        itemToReturn = @"have some gum";
+    else if (dollars < 1000)
+        itemToReturn = @"have an apple";
+    else if (dollars < 1000000000)
+        itemToReturn = @"have an Apple computer";
+    else // dollars >= 1000000000
+        itemToReturn = @"have The Big Apple";
+    
     return itemToReturn;
 }
 
@@ -24,10 +36,13 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
-    
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
+
+    // self.getsDiscount ? cost=18 : cost=24; // oops, doesn't work
+    cost = self.getsDiscount ? cost*=.75 : cost;
+
     return cost;
 }
 
