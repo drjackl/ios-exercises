965d7a3d36b948cda4b2acb6341643a3a3b53e2f
Jack Li <drjackl@gmail.com>
2016-02-19 09:23:36 -0800

    Finished assignment 14 Dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,27 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    if (characterDictionary != nil) {
+        return characterDictionary[@"favorite drink"];
+    }
+    return nil;//@"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *favoriteDrinksArray = [NSMutableArray arrayWithCapacity:charactersArray.count];
+    [charactersArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        //favoriteDrinksArray[idx] = charactersArray[idx][@"favorite drink"]; // this works too
+        favoriteDrinksArray[idx] = obj[@"favorite drink"]; // I don't have to cast?
+    }];
+    return favoriteDrinksArray;//@[];
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *mutableCharacterDictionary = [NSMutableDictionary dictionaryWithDictionary:characterDictionary];
+    mutableCharacterDictionary[@"quote"] = @"*Insert memorable quote*";
+    return mutableCharacterDictionary;//@{};
 }
 
 @end
